9828b841959e2d43243bd8a601376909d70b0793
Ethan-Patrick5 <epatrick504@gmail.com>
2025-10-20 22:28:59 -0400

    Updated scanner

--- Scanner.py
+++ Scanner.py
@@ -1,6 +1,13 @@
 import docscanner
+import cv2
+import Search
 
 class Scanner:
     
     def __init__(resume):
         Scanner.resume = resume
+        
+    def get_skills(self):
+        docscanner.initlicense("DKU21-S6650-GHOP7-RELT3-28FCD")
+        scanner = docscanner.createInstance()
+        results = scanner
