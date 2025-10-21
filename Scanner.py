994355bf82a32bd389655d7183a9632bfd6502da
Ethan-Patrick5 <epatrick504@gmail.com>
2025-10-21 09:51:39 -0400

    Started adding scanner to scan resume

--- Scanner.py
+++ Scanner.py
@@ -2,6 +2,7 @@ import docscanner
 import cv2
 import Search
 
+
 class Scanner:
     
     def __init__(resume):
@@ -10,4 +11,6 @@ class Scanner:
     def get_skills(self):
         docscanner.initlicense("DKU21-S6650-GHOP7-RELT3-28FCD")
         scanner = docscanner.createInstance()
-        results = scanner
+        image = scanner.capture_image(Scanner.resume)
+        scanner_resume = scanner.scan(image)
+        
